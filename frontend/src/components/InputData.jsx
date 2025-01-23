301a29d1194674d42dcc3561a29a0c5e29a8fce9
Kamran Hossain Topu <117673483+MuhammadTopu@users.noreply.github.com>
2025-01-24 01:26:36 +0600

    updated in frontend

--- frontend/src/components/InputData.jsx
+++ frontend/src/components/InputData.jsx
@@ -9,7 +9,7 @@ const InputData = ({setInputDiv}) => {
     <div className={`fixed left-0 top-0 flex items-center justify-center  h-screen w-full`}>
     
       <div className='w-2/6 bg-gray-900 p-4 rounded text-black'>
-      <div className='flex justify-end text-2xl hover:text-white' onClick={()=> setInputDiv("hidden")}><RxCross2 /></div>
+      <div className='flex justify-end text-2xl hover:text-white' onClick={()=> setInputDiv("hidden")}><RxCross2 className="hover:cursor-pointer" /></div>
      <input type="text" placeholder='Title' name='title' className='p-2 rounded w-full my-3'/>
      <textarea name="disc" id="" cols={10} rows={5}  placeholder='Enter the discription' className='p-2 rounded w-full my-3 resize-none' ></textarea>
      <button className='p-2 rounded bg-blue-400 hover:bg-blue-700 transition-all duration-300 hover:text-white'>Submit</button>
